a8e01f6d0305bd7617936dae2b02c8186e5a4d15
Borroot <borroot@live.nl>
2020-05-31 00:11:49 +0200

    Add terminating character.

--- src/x_string/x_strndup.c
+++ src/x_string/x_strndup.c
@@ -5,5 +5,10 @@
 char *x_strndup(const char *s, size_t n)
 {
 	char *dup = (char *)malloc(x_strnlen(s, n) + 1);
-	return (dup == NULL) ? NULL : x_strncpy(dup, s, n);
+
+	if (dup != NULL) {
+		x_strncpy(dup, s, n);
+		dup[n] = '\0';
+	}
+	return dup;
 }
