a384b863d1520b26942eee23d984d05659d076cd
Borroot <borroot@live.nl>
2020-06-12 22:47:46 +0200

    Add stdio and stdlib header files.

---
+++ inc/bt_stdio.h
@@ -0,0 +1,14 @@
+#ifndef BT_STDIO_H_
+#define BT_STDIO_H_
+
+//void bt_putchar(char);
+//void bt_putstr(const char *);
+//void bt_putendl(const char *);
+//void bt_putdigit(int);
+
+//void bt_putchar_fd(char, int);
+//void bt_putstr_fd(const char *, int);
+//void bt_putendl_fd(const char *, int);
+//void bt_putdigit_fd(int, int);
+
+#endif /* BT_STDIO_H_ */
---
+++ inc/bt_stdlib.h
@@ -0,0 +1,6 @@
+#ifndef BT_STDLIB_H_
+#define BT_STDLIB_H_
+
+//char *bt_itoa(int);
+
+#endif /* BT_STDLIB_H_ */
