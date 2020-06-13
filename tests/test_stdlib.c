385cd961c24f4900f3368b622d456dca72b8a5f7
Borroot <borroot@live.nl>
2020-06-14 00:53:09 +0200

    Add hex test case.

--- tests/test_stdlib.c
+++ tests/test_stdlib.c
@@ -9,4 +9,5 @@ Test(stdlib, itoa)
 	cr_assert_str_eq(bt_itoa(-1), "-1");
 	cr_assert_str_eq(bt_itoa( 1234567890), "1234567890");
 	cr_assert_str_eq(bt_itoa(-1234567890), "-1234567890");
+	cr_assert_str_eq(bt_itoa(0xFF), "255");
 }
